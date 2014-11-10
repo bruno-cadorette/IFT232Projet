9771b1d7a2b1cd13eddc0c0c7a2437ddfd5649b9
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2014-11-10 17:48:24 -0500

    Fred et Christo 2

---
+++ ProjetIft232/Buildings/BuildingFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetIft232.Buildings
+{
+    public class BuildingFactory
+    {
+        public static Building CreateBuilding(BuildingType type, City city)
+        {
+            Building building = GetBuilding(type);
+            if (building != null && building.CanBeBuild(city.Ressources, city.Buildings))
+            {
+                city.RemoveResources(building.Requirement.Resources);
+                return building;
+            }
+            return null;
+        }
+
+        private static Building GetBuilding(BuildingType type)
+        {
+            return new Building(BuildingLoader.getInstance()._buildings[type]);
+
+        }
+    }
+}
---
+++ ProjetIft232/Buildings/Requirement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetIft232.Buildings
+{
+    public class Requirement
+    {
+        private IEnumerable<BuildingType> _Buildings;
+        public Resource Resources { get; private set; }
+
+        public Requirement(IEnumerable<BuildingType> buildings, Resource resources)
+        {
+            _Buildings = buildings;
+            Resources = resources;
+        }
+
+        public static Requirement Zero()
+        {
+            return new Requirement(new BuildingType[0], Resource.Zero());
+        }
+
+
+        public bool IsValid(Resource actualResource, IEnumerable<Building> actualBuildings)
+        {
+            return actualResource >= Resources && _Buildings.All(type =>
+                actualBuildings.Any(x => 
+                    x.Type == type && !x.InConstruction)
+                );
+        }
+    }
+}
