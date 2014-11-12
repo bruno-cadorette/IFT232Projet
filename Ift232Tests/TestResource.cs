5b274d56de995bde7d44b0aa8938f874a699e63e
lavabre-m <einsteinefolle@hotmail.fr>
2014-11-13 00:05:13 +0100

    Update of resources to futur market addition

--- Ift232Tests/TestResource.cs
+++ Ift232Tests/TestResource.cs
@@ -13,125 +13,126 @@ namespace Ift232Tests
         [TestMethod]
         public void TestConstructeur()
         {
-            Dictionary<Resources, int> rsc = new Dictionary<Resources, int>();
-            rsc.Add(Resources.Gold, 1);
-            Resource res = new Resource();
-            Resource res2 = new Resource(rsc);
+            Dictionary<ResourcesType, int> rsc = new Dictionary<ResourcesType, int>();
+            rsc.Add(ResourcesType.Gold, 1);
+            Resources res = new Resources();
+            Resources res2 = new Resources(rsc);
 
-            Assert.AreEqual(res2, new Resource(rsc));
-
-
-            rsc.Add(Resources.Meat, 2);
-            Resource res3 = new Resource(rsc);
-
-            Assert.AreEqual(res3, new Resource(rsc));
+            Assert.AreEqual(res2, new Resources(rsc));
 
 
-            rsc.Add(Resources.Rock, 3);
-            Resource res4 = new Resource(rsc);
+            rsc.Add(ResourcesType.Meat, 2);
+            Resources res3 = new Resources(rsc);
 
-            Assert.AreEqual(res4, new Resource(rsc));
-
-
-            rsc.Add(Resources.Wood, 5);
-            Resource res5 = new Resource(rsc);
-
-            Assert.AreEqual(res5, new Resource(rsc));
+            Assert.AreEqual(res3, new Resources(rsc));
 
 
-            rsc.Add(Resources.Population, 16);
-            Resource res6 = new Resource(rsc);
-            Assert.IsTrue(res == new Resource() && res != res2 && res != res3 && res != res4 && res != res5 && res != res6);
-            Assert.AreEqual(res6, new Resource(rsc));
+            rsc.Add(ResourcesType.Rock, 3);
+            Resources res4 = new Resources(rsc);
+
+            Assert.AreEqual(res4, new Resources(rsc));
+
+
+            rsc.Add(ResourcesType.Wood, 5);
+            Resources res5 = new Resources(rsc);
+
+            Assert.AreEqual(res5, new Resources(rsc));
+
+
+            rsc.Add(ResourcesType.Population, 16);
+            Resources res6 = new Resources(rsc);
+            Assert.IsTrue(res == new Resources() && res != res2 && res != res3 && res != res4 && res != res5 && res != res6);
+            Assert.AreEqual(res6, new Resources(rsc));
 
         }
 
         [TestMethod]
         public void TestAdd()
         {
-            Dictionary<Resources,int> rsc = new Dictionary<Resources, int>();
-            rsc.Add(Resources.Gold, 1);
-            Resource res = new Resource(rsc);
-            Resource res2 = new Resource(rsc);
-            Resource rs3 = res + res2;
+            Dictionary<ResourcesType,int> rsc = new Dictionary<ResourcesType, int>();
+            rsc.Add(ResourcesType.Gold, 1);
+            Resources res = new Resources(rsc);
+            Resources res2 = new Resources(rsc);
+            Resources rs3 = res + res2;
 
-            Resource rs4 = res - res2;
-            Assert.IsTrue(rs3.Gold == 2);
-            Assert.IsTrue(rs4.Gold == 0);
-            Assert.IsTrue( rs3.Meat == 0 && rs3.Rock == 0 && rs3.Population == 0 && rs3.Wood == 0);
-            Assert.IsTrue(rs4.Meat == 0 && rs4.Rock == 0 && rs4.Population == 0 && rs4.Wood == 0);
+            Resources rs4 = res - res2;
+            Assert.IsTrue(rs3.get("Gold") == 2);
+            Assert.IsTrue(rs4.get("Gold") == 0);
+            Assert.IsTrue( rs3.get("Meat") == 0 && rs3.get("Rock") == 0 && rs3.get("Population") == 0 && rs3.get("Wood") == 0);
+            Assert.IsTrue(rs4.get("Meat") == 0 && rs4.get("Rock") == 0 && rs4.get("Population") == 0 && rs4.get("Wood") == 0);
         }
 
         [TestMethod]
         public void TestEqual()
         {
-            Dictionary<Resources, int> rsc = new Dictionary<Resources, int>();
-            rsc.Add(Resources.Gold, 1);
+            Dictionary<ResourcesType, int> rsc = new Dictionary<ResourcesType, int>();
+            rsc.Add(ResourcesType.Gold, 1);
 
-            Resource rsc2 = new Resource();
+            Resources rsc2 = new Resources();
             
-            Resource res = new Resource(rsc);
+            Resources res = new Resources(rsc);
 
             Assert.IsFalse(rsc2.Equals(null));
             Assert.IsFalse(rsc2.Equals(new object()));
-            Assert.IsTrue(rsc2.Equals(new Resource()));
-            Assert.IsTrue(rsc2 == new Resource());
+            Assert.IsTrue(rsc2.Equals(new Resources()));
+            Assert.IsTrue(rsc2 == new Resources());
             Assert.IsFalse(rsc2 == res);
             Assert.IsTrue(rsc2 != res);
-            Assert.IsFalse(rsc2 != new Resource());
+            Assert.IsFalse(rsc2 != new Resources());
         }
 
         [TestMethod]
         public void TestGreater()
         {
-            Dictionary<Resources, int> rsc = new Dictionary<Resources, int>();
-            rsc.Add(Resources.Gold, 1);
+            Dictionary<ResourcesType, int> rsc = new Dictionary<ResourcesType, int>();
+            rsc.Add(ResourcesType.Gold, 1);
 
-            Resource rsc2 = new Resource();
+            Resources rsc2 = new Resources();
 
-            Resource res = new Resource(rsc);
+            Resources res = new Resources(rsc);
 
-            Assert.IsFalse(rsc2 > new Resource());
-            Assert.IsFalse(rsc2 < res);
-            Assert.IsFalse(res > rsc2);
-            Assert.IsFalse(new Resource() > rsc2);
+            Assert.IsFalse(rsc2 > new Resources());
+            Assert.IsFalse(rsc2 > res);
+            Assert.IsFalse(res < rsc2);
+            Assert.IsFalse(new Resources() > rsc2);
         }
 
         [TestMethod]
         public void TestGreaterOrEquals()
         {
-            Dictionary<Resources, int> rsc = new Dictionary<Resources, int>();
-            rsc.Add(Resources.Gold, 1);
+            Dictionary<ResourcesType, int> rsc = new Dictionary<ResourcesType, int>();
+            rsc.Add(ResourcesType.Gold, 1);
 
-            Resource rsc2 = new Resource();
+            Resources rsc2 = new Resources();
 
-            Resource res = new Resource(rsc);
+            Resources res = new Resources(rsc);
 
-            Assert.IsTrue(rsc2 >= new Resource());
+            Assert.IsTrue(rsc2 >= new Resources());
             Assert.IsFalse(rsc2 >= res);
             Assert.IsFalse(res <= rsc2);
-            Assert.IsTrue(new Resource() >= rsc2);
+            Assert.IsTrue(new Resources() >= rsc2);
         }
 
         [TestMethod]
         public void TestUpdate()
         {
-            Dictionary<Resources, int> rsc = new Dictionary<Resources, int>();
-            rsc.Add(Resources.Gold, 1);
+            City city = new City("test");
+            Dictionary<ResourcesType, int> rsc = new Dictionary<ResourcesType, int>();
+            rsc.Add(ResourcesType.Gold, 1);
 
-            Resource rsc2 = new Resource();
+            city.RemoveResources(new Resources(10000,10000,10000,10000,10000));
 
-            Resource res = new Resource(rsc);
-            Dictionary<Resources, int> rsc6 = new Dictionary<Resources, int>();
-            rsc6.Add(Resources.Gold, 5);
-            rsc6.Add(Resources.Meat, 5);
-            rsc6.Add(Resources.Wood, 5);
-            rsc6.Add(Resources.Rock, 5);
-            rsc6.Add(Resources.Population, 1);
+            Resources res = new Resources(rsc);
+            Dictionary<ResourcesType, int> rsc6 = new Dictionary<ResourcesType, int>();
+            rsc6.Add(ResourcesType.Gold, 5);
+            rsc6.Add(ResourcesType.Meat, 5);
+            rsc6.Add(ResourcesType.Wood, 5);
+            rsc6.Add(ResourcesType.Rock, 5);
+            rsc6.Add(ResourcesType.Population, 1);
 
-            Assert.AreEqual(rsc2, new Resource());
-            rsc2.Update(new Resource());
-            Assert.AreEqual(rsc2, new Resource(rsc6));
+            Assert.AreEqual(city.Ressources, new Resources());
+            city.Update();
+            Assert.AreEqual(city.Ressources, new Resources(rsc6));
         }
     }
 }
