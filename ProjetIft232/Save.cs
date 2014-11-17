39e24a06e5bf73be16be5d3cd40044ac21421868
anfray-lapeyre <aapocalipso08@gmail.com>
2014-11-16 21:57:49 -0500

    Rajout de save.cs (Anfray et Christopher)

---
+++ ProjetIft232/Save.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetIft232
+{
+    public class Save
+    {
+        public String title;
+
+        public Save()
+        {
+
+        }
+
+    }
+}
