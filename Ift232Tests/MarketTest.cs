83cdec57f9767e4600a0d7b5eb184786dd5347cc
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2014-11-18 20:02:22 -0500

    Fred et Melannie debut du Market

---
+++ Ift232Tests/MarketTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetIft232;
+using ProjetIft232.Buildings;
+
+namespace Ift232Tests
+{
+    [TestClass]
+    public class MarketTest
+    {
+        public City city1 { get; set; }
+        public Market market { get; set; }
+
+
+
+        [TestInitialize()]
+        public void Init()
+        {
+            city1 = new City("Toulouse");
+            market = new Market(Resources.Zero());
+
+        }
+        [TestMethod]
+        public void TestExchangeWood()
+        {
+            Assert.IsTrue(market.Exchange(city1,ResourcesType.Wood, 20));
+
+        }
+    }
+}
--- ProjetIft232/Buildings/Market.cs
+++ ProjetIft232/Buildings/Market.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +9,56 @@ namespace ProjetIft232.Buildings
 {
     public class Market : Building
     {
+        private Resources resources;
+        public int RatioBois { get; private set; }
+        public int RatioViande { get; private set; }
+        public int RatioPierre{ get; private set; }
 
-        
+
         public Market( Resources MarketResources)
             : base(3)
         {
+            resources = new Resources(100000, 100000, 100000, 100000, 0);
+            this.RatioBois = 15;
+            this.RatioPierre = 12;
+            this.RatioViande = 8;
+
             Name = "Marché";
             Description =
                 "Un bâtiment permettant l'échange de ressources.\n Qui sait, peut-être que si vous aidez le marchand il vous donnera un peu d'or...\n";
             
         }
+
+        public bool Exchange( City city, ResourcesType resourceWanted, int amountGold)
+        {
+            if (amountGold >= 1) { 
+           
+            switch(resourceWanted)
+            {
+                case ResourcesType.Wood:
+                    city.AddResources(new Resources(amountGold*RatioBois,0,0,0,0));
+                   
+                    break;
+                case ResourcesType.Meat:
+                    city.AddResources(new Resources(0, 0, amountGold*RatioViande, 0, 0));
+                    
+                    break;
+
+                case ResourcesType.Rock:
+                    city.AddResources(new Resources(0, 0,0, amountGold * RatioPierre, 0));
+                    
+
+                    break;
+            }
+            city.RemoveResources(new Resources(0, amountGold, 0, 0, 0));
+                return true;
+
+             
+
+            }
+            return false;
+        }
+
 
 
         // Création d'un historique pour changer les prix en fonction de l'offre et la demande
