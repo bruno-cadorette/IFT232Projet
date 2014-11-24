b90df82b40a7546e1e3533c305e9ce256f3ef0c6
Bruno Cadorette <brunocadorette@outlook.com>
2014-11-24 13:58:32 -0500

    Correction du commit (army loader)

---
+++ Ift232Tests/LoaderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetIft232;
+using ProjetIft232.Buildings;
+
+namespace Ift232Tests
+{
+    [TestClass]
+    public class LoaderTest
+    {
+        [TestMethod]
+        public void RequirementTest()
+        {
+            var casern = BuildingLoader.GetInstance().GetBuilding((int)BuildingType.Casern);
+            var builds = new int[] { 0, 1 };
+            var requirements = new Requirement(builds, new Resources(15, 20, 25, 15, 10));
+            Assert.AreEqual(requirements.Resources, casern.Requirement.Resources);
+            var a1 = requirements.Buildings.ToArray();
+            for (int i = 0; i < builds.Length; i++)
+            {
+                Assert.AreEqual(builds[i], a1[i]);
+            }
+            Assert.AreEqual(builds.Length, a1.Length);
+            Assert.AreEqual(requirements.Resources, casern.Requirement.Resources);
+        }
+    }
+}
---
+++ ProjetIft232/Army/ArmyLoader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ProjetIft232.Army
+{
+    public class ArmyLoader : Loader
+    {
+        private static ArmyLoader instance;
+
+        private static object SyncRoot = new object();
+
+
+        protected override IEnumerable<XElement> GetChilds(XElement root)
+        {
+            return root.Element(XName.Get("Soldiers")).Elements(XName.Get("Soldier"));
+        }
+
+        public ArmyUnit GetSoldier(int type)
+        {
+            return new ArmyUnit((ArmyUnit)_entities[type]);
+        }
+
+        public IEnumerable<ArmyUnit> Soldiers()
+        {
+            return _entities.Select(x => (ArmyUnit)x.Value);
+        }
+
+        public IEnumerable<ArmyUnit> ArmyUnit()
+        {
+            return _entities.Select(x => (ArmyUnit)x.Value);
+        }
+
+        protected override BuildableEntity CreateEntity(XElement element, int id, string name, string description, int turns, Requirement requirement)
+        {
+            Resources resources = GetResources(element);
+            string specialType = Special(element);
+            int Attack = int.Parse(GetAttribute(element, "Attack"));
+            int Defense = int.Parse(GetAttribute(element, "Defense"));
+            ArmyUnit army = new ArmyUnit(id, name, description,Attack,Defense, turns, resources, requirement);
+
+            switch (specialType)
+            {
+                case "General":
+                default:
+                    return army;
+            }
+
+        }
+
+        public static ArmyLoader GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (SyncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ArmyLoader();
+                    }
+                }
+            }
+            return instance;
+        }
+
+    }
+}
