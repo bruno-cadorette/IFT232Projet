006d55cd3019fba5857a0954aaa443cf7c749c98
Bruno Cadorette <brunocadorette@outlook.com>
2014-12-31 02:17:56 -0500

    Generateur des tuiles a partir d'un fichier

---
+++ GameBuilder/Utility/TileSetGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace GameBuilder.Utility
+{
+    class TileSetGenerator
+    {
+        private BitmapImage tileset;
+        public TileSetGenerator(string path)
+        {
+            tileset = new BitmapImage(new Uri(path));
+        }
+        public IEnumerable<BitmapSource> GetTiles(int height, int width)
+        {
+            for (int y = 0; y < tileset.PixelHeight; y += height)
+            {
+                for (int x = 0; x < tileset.PixelWidth; x += width)
+                {
+                    Int32Rect area = new Int32Rect(x, y, width, height);
+                    yield return new CroppedBitmap(tileset, area);
+                    
+                }
+            }
+        }
+    }
+}
